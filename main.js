0df2a8a19fc1ed7b9406631fe79bad4b2865c2a1
Emil Bergold <emil.bergold@gmail.com>
2014-11-23 11:56:22 +0100

    spinner on splash

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 window.addEventListener('polymer-ready', function(e) {
 
   var app = document.querySelector('flow-app');
-  app.$.pagehost.selected = 'app';
+  //app.$.pagehost.selected = 'app';
 
   app.shadowRoot.querySelector("flow-app-details").$.navigation.selected = "dynos"
 
