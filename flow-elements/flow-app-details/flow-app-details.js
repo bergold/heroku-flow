ba9914cad72db7313ba22b91c70366bfecd85b58
Emil Bergold <emil.bergold@gmail.com>
2014-11-24 19:37:18 +0100

    add app attr

--- flow-elements/flow-app-details/flow-app-details.js
+++ flow-elements/flow-app-details/flow-app-details.js
@@ -3,9 +3,10 @@ Polymer('flow-app-details', {
   domReady: function() {
 
     this.$.toggleNav.addEventListener('click', this.toggleAppsnav.bind(this));
-    this.$.navdrawer.addEventListener('core-select', this.closeAppsnav.bind(this));
-    this.$.navigation.addEventListener('core-select', this.selectPage.bind(this));
     this.$.appsnav.addEventListener('core-collapse-open', this.updateToggleButtonIcon.bind(this));
+    this.$.appsnav.addEventListener('core-select', this.selectApp.bind(this));
+    this.$.navigation.addEventListener('core-select', this.selectPage.bind(this));
+    this.$.navdrawer.addEventListener('core-select', this.closeAppsnav.bind(this));
 
   },
 
@@ -25,6 +26,15 @@ Polymer('flow-app-details', {
   updateToggleButtonIcon: function() {
     if (this.$.appsnav.opened) this.$.toggleNav.icon = 'arrow-drop-up';
     else this.$.toggleNav.icon = 'arrow-drop-down';
+  },
+
+  selectApp: function() {
+    // selection in appsnav changed
+    this.$.navdrawer.closeDrawer();
+  },
+
+  appChanged: function(oldval, newVal) {
+    this.$.appsnav.querySelector('core-menu').selected = newVal;
   }
 
 });
