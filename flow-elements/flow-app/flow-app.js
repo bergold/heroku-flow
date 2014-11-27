93ddf045e78209445fc315342987854e45d0af14
Emil Bergold <emil.bergold@gmail.com>
2014-11-27 15:23:31 +0100

    call show after page transitioned

--- flow-elements/flow-app/flow-app.js
+++ flow-elements/flow-app/flow-app.js
@@ -1,13 +1,18 @@
 Polymer('flow-app', {
 
   domReady: function() {
-
+    this.$.pagehost.addEventListener('core-animated-pages-transition-end', this.pageLoadEnd.bind(this));
   },
 
   goto: function(page, app) {
     this.$.pagehost.selected = page;
     this.$.apps.app = app;
     this.$.app.app = app;
+  },
+
+  pageLoadEnd: function() {
+    var el = this.$.pagehost.selectedItem;
+    if (el && el.show) el.show();
   }
 
 });
--- flow-elements/flow-apps/flow-apps.js
+++ flow-elements/flow-apps/flow-apps.js
@@ -1,17 +1,13 @@
 Polymer('flow-apps', {
 
+  show: function() {
+    var items = this.shadowRoot.querySelectorAll('paper-item');
+    items.array().forEach(function(item) { item.removeAttribute('hero'); });
+  },
+
   onAppTap: function(e, d, s) {
     s.setAttribute('hero', '');
     this.fire('app-select', s.getAttribute('data-app'));
-  },
-
-  liftup: function(e, d, s) {
-    s.style.zIndex = 1;
-    s.setZ(1);
-  },
-  liftdown: function(e, d, s) {
-    s.style.zIndex = 0;
-    s.setZ(0);
   }
 
 });
